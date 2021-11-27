b6c4a49b04567c4ed46cf01b4617860a995e5656
algonacci <algonacci1@gmail.com>
2021-11-27 20:59:33 +0700

    Updates using new API
    Made adjustments for Heroku deployments

--- routers/crowdfundingsRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const route = express.Router();
-const { crowdfundings } = require('../controllers/crowdfundingsController');
-
-route.get('/crowdfundings', crowdfundings);
-
-module.exports = route;
